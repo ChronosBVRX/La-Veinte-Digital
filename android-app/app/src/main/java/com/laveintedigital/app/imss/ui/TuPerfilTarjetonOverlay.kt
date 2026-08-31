aca80ef5e100eff4359ff078f792abbd53b16165
ChronosBVRX <noirsysan@gmail.com>
2026-08-31 17:52:01 -0600

    fix(android): add IME insets to native IMSS dialogs and overlays

--- android-app/app/src/main/java/com/laveintedigital/app/imss/ui/TuPerfilTarjetonOverlay.kt
+++ android-app/app/src/main/java/com/laveintedigital/app/imss/ui/TuPerfilTarjetonOverlay.kt
@@ -11,12 +11,17 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.ime
+import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.union
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
@@ -77,7 +82,8 @@ fun TuPerfilTarjetonOverlay(
         Box(
             modifier = Modifier
                 .fillMaxSize()
-                .background(LvdColors.Scrim),
+                .background(LvdColors.Scrim)
+                .windowInsetsPadding(WindowInsets.navigationBars.union(WindowInsets.ime)),
         ) {
             LvdBottomSheet(
                 modifier = Modifier
