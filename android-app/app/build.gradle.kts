74b6428945cd118f16be48efd8e43be651bfa8e7
ChronosBVRX <noirsysan@gmail.com>
2026-08-31 17:52:05 -0600

    chore(release): bump version to 1.0.99 (199)

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.laveintedigital.app"
         minSdk = 29
         targetSdk = 36
-        versionCode = 198
-        versionName = "1.0.98"
+        versionCode = 199
+        versionName = "1.0.99"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
